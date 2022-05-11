585db604a74a33f402da14ea6316248d650bd45d
duc.bui.trung <dG5TJnIT>
2022-05-11 14:01:47 +0700

    BTVN

---
+++ layouts/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Solid Template</title>
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/style.css">
+	<script src="https://unpkg.com/animejs@3.0.1/lib/anime.min.js"></script>
+    <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+</head>
+<body class="is-boxed has-animations">
+    <div class="body-wrap">
+        <!-- Header -->
+     <?php include_once 'header.php' ?>
+       <!-- main -->
+     <?php include_once 'main.php' ?>
+       <!-- footer -->
+     <?php include_once 'footer.php' ?>
+    </div>
+
+    <script src="dist/js/main.min.js"></script>
+</body>
+</html>
